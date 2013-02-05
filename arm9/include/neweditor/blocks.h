4c57debcfadd76fa9515953c874dbd47ed55a4bc
jordan rabet <rabetj@ensimag.imag.fr>
2013-02-05 13:55:39 +0100

    ajoute des trucs

--- arm9/include/neweditor/blocks.h
+++ arm9/include/neweditor/blocks.h
@@ -1,12 +1,24 @@
 #ifndef BLOCKS_H
 #define BLOCKS_H
 
+#define ROOMARRAYSIZEX (64)
+#define ROOMARRAYSIZEY (64)
+#define ROOMARRAYSIZEZ (64)
+
 typedef struct blockFace_struct
 {
 	u8 x, y, z;
 	u8 direction;
 	struct blockFace_struct* next;
 }blockFace_struct;
+
+typedef struct
+{
+	u8* blockArray;
+	blockFace_struct* blockFaceList;
+}editorRoom_struct;
+
+void initEditorRoom(editorRoom_struct* er);
 
 void initBlocks(void);
 blockFace_struct* popBlockFace(blockFace_struct** l);
--- arm9/source/neweditor/blocks.c
+++ arm9/source/neweditor/blocks.c
@@ -7,6 +7,43 @@ void initBlocks(void)
 	blockFacePool=NULL;
 }
 
+u8 getBlock(u8* ba, s8 x, s8 y, s8 z)
+{
+	if(!ba || x<0 || y<0 || z<0 || x>=ROOMARRAYSIZEX || y>=ROOMARRAYSIZEY || z>=ROOMARRAYSIZEZ)return 1; //not empty
+	return ba[x+y*ROOMARRAYSIZEX+z*ROOMARRAYSIZEX*ROOMARRAYSIZEY];
+}
+
+void setBlock(u8* ba, u8 x, u8 y, u8 z, u8 v)
+{
+	if(!ba || x<0 || y<0 || z<0 || x>=ROOMARRAYSIZEX || y>=ROOMARRAYSIZEY || z>=ROOMARRAYSIZEZ)return;
+	ba[x+y*ROOMARRAYSIZEX+z*ROOMARRAYSIZEX*ROOMARRAYSIZEY]=v;
+}
+
+void initBlockArray(u8* ba)
+{
+	if(!ba)return;
+	int i, j, k;
+	for(i=0;i<ROOMARRAYSIZEX;i++)
+	{
+		for(j=0;j<ROOMARRAYSIZEY;j++)
+		{
+			for(k=0;k<ROOMARRAYSIZEZ;k++)
+			{
+				if(i<16 || j<16 || k<16 || i>=48 || j>=48 || k>=48)setBlock(ba,i,j,k,1);
+				else setBlock(ba,i,j,k,0);
+			}
+		}
+	}
+}
+
+void initEditorRoom(editorRoom_struct* er)
+{
+	if(!er)return;
+	er->blockFaceList=NULL;
+	er->blockArray=malloc(sizeof(u8)*ROOMARRAYSIZEX*ROOMARRAYSIZEY*ROOMARRAYSIZEZ);
+	initBlockArray(er->blockArray);
+}
+
 blockFace_struct* popBlockFace(blockFace_struct** l)
 {
 	if(!l || !*l)return NULL;
@@ -40,4 +77,9 @@ void addBlockFace(blockFace_struct** l, blockFace_struct* bf)
 	*l=bf;
 }
 
-
+void generateBlockFaces(u8* ba, blockFace_struct** l, u8 x, u8 y, u8 z)
+{
+	if(!ba || !l || !*l || x>=ROOMARRAYSIZEX || y>=ROOMARRAYSIZEY || z>=ROOMARRAYSIZEZ)return;
+	
+	if()addBlockFace(l, createBlockFace(x,y,z,));
+}
